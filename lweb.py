f9d918516342a7664560227ed1b106db6f2fa3ab
StackNeverflow <50374560+StackNeverFlow@users.noreply.github.com>
2023-09-17 08:31:49 +0200

    Update *

--- lweb.py
+++ lweb.py
@@ -148,16 +148,16 @@ app.layout = html.Div([
     html.Div([
     html.Br(),
     html.Br(),
-    dcc.Input(id = 'o', value = 'Av Desarrollo 1801, Complejo Industrial Impulso, 31183 Chihuahua, Chih., Mexico', type = 'text', persistence=False),
-    dcc.Input(id = 'd', value = 'Blvd. Adolfo López Mateos 909 Ote. Esq. con, Av Tamaulipas, Valle de Aguayo, 87020 Cd Victoria, Tamps., Mexico', type = 'text', persistence=False),
-    dcc.Input(id = 'tonnage', value = 32, type = 'number', persistence=False),
+    dcc.Input(id = 'o', value = 'Av Desarrollo 1801, Complejo Industrial Impulso, 31183 Chihuahua, Chih., Mexico', placeholder="Origin address", type = 'text', persistence=False),
+    dcc.Input(id = 'd', value = 'Blvd. Adolfo López Mateos 909 Ote. Esq. con, Av Tamaulipas, Valle de Aguayo, 87020 Cd Victoria, Tamps., Mexico', placeholder="Destination address", type = 'text', persistence=False),
+    dcc.Input(id = 'tonnage', placeholder="Tonnage (t)", type = 'number', persistence=False),
     html.Button('Register', id='btn-nclicks-1', n_clicks=0),
     ], id="element-to-hide"),
     html.Br(),
     html.Br(),
-    dcc.Input(id = 'w_s', value = '67806 Linares, Nuevo Leon, Mexico', type = 'text'),
-    dcc.Input(id = 'w_e', value = 'Blvd Harold R. Pape 1202, La Loma, 25770 Monclova, Coah., Mexico', type = 'text', persistence=False),
-    dcc.Input(id = 'tonnage2', value = 15, type = 'number', persistence=False),
+    dcc.Input(id = 'w_s', value = '67806 Linares, Nuevo Leon, Mexico', placeholder="Cargo pickup", type = 'text'),
+    dcc.Input(id = 'w_e', value = 'Blvd Harold R. Pape 1202, La Loma, 25770 Monclova, Coah., Mexico', placeholder="Cargo destination", type = 'text', persistence=False),
+    dcc.Input(id = 'tonnage2', placeholder="Tonnage (t)", type = 'number', persistence=False),
     html.Button('Suggest', id='btn-nclicks-2', n_clicks=0)
 ])
 
@@ -180,7 +180,10 @@ def update_output(btn1, btn2, o, d, w_s, w_e, tonnage, tonnage2):
     fig_map1.add_trace((go.Scattermapbox(name="Planned Itinerary (" + co2_1 + " kg CO2)", mode = "lines", lat = df["x"].tolist(), lon = df["y"].tolist(), marker=go.scattermapbox.Marker(color='blue'))))
     fig_map1.add_trace((go.Scattermapbox(name="Empty truck (" + co2_2 + " kg CO2)", mode = "lines", lat = df_b["x"].tolist(), lon = df_b["y"].tolist(), marker=go.scattermapbox.Marker(color='red'))))
 
-    fig_map1.update_layout(margin ={'l':10,'t':10,'b':10,'r':10}, mapbox = {'center': {'lon': -100, 'lat': 25.5}, 'style': "open-street-map", 'zoom': 6},height=700)
+    lat1=df["x"].mean()
+    lon1=df["y"].mean()
+
+    fig_map1.update_layout(margin ={'l':10,'t':10,'b':10,'r':10}, mapbox = {'center': {'lon': lon1, 'lat': lat1}, 'style': "open-street-map", 'zoom': 6},height=700)
     return dcc.Graph(figure=fig_map1, id='map'), html.Br(), html.Br()
 
   if "btn-nclicks-2" == ctx.triggered_id:
@@ -190,6 +193,10 @@ def update_output(btn1, btn2, o, d, w_s, w_e, tonnage, tonnage2):
     df_b = pd.DataFrame(createSimpleRoute(o, d, True), columns =['x', 'y'])
     co2_1 = calculateCO2(o, d, False, tonnage)
     co2_2 = calculateCO2(o, d , True, 1)
+
+    lat1=df_b2["x"].mean()
+    lon1=df_b2["y"].mean()
+    
     fig_map1.add_trace((go.Scattermapbox(name="Planned Itinerary (" + co2_1 + " kg CO2)", mode = "lines", lat = df["x"].tolist(), lon = df["y"].tolist(), marker=go.scattermapbox.Marker(color='blue'))))
     fig_map1.add_trace((go.Scattermapbox(name="Empty truck (" + co2_2 + " kg CO2)", mode = "lines", lat = df_b["x"].tolist(), lon = df_b["y"].tolist(), marker=go.scattermapbox.Marker(color='red'))))
 
@@ -197,7 +204,7 @@ def update_output(btn1, btn2, o, d, w_s, w_e, tonnage, tonnage2):
 
     fig_map1.add_trace((go.Scattermapbox(name="Suggest Path (" + co2_3 + " kg CO2)", mode = "lines", lat = df_b2["x"].tolist(), lon = df_b2["y"].tolist(), marker=go.scattermapbox.Marker(color='green'))))
     
-    fig_map1.update_layout(margin ={'l':10,'t':10,'b':10,'r':10}, mapbox = {'center': {'lon': -100, 'lat': 25.5}, 'style': "open-street-map", 'zoom': 6},height=700)
+    fig_map1.update_layout(margin ={'l':10,'t':10,'b':10,'r':10}, mapbox = {'center': {'lon': lon1, 'lat': lat1}, 'style': "open-street-map", 'zoom': 6},height=700)
     return dcc.Graph(figure=fig_map1, id='map'), html.Br(), html.Br()
 
 @app.callback(Output(component_id='element-to-hide', component_property='style'), [Input(component_id='dropdown-to-show_or_hide-element', component_property='value')])
@@ -208,4 +215,4 @@ def show_hide_element(visibility_state):
     return {'display': 'none'}
 
 if __name__ == "__main__":
-    app.run_server(debug=True, threaded=True, host="0.0.0.0")
+    app.run_server(threaded=True, host="0.0.0.0")
